790368b7c108ad15801a1863413778a72d89b1e9
Reetika-786 <reetika848@gmail.com>
2025-11-03 11:18:35 +0530

    created signUp page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,16 @@ import { ToastContainer } from 'react-toastify';
 
 import Home from './pages/Home';
 import Login from './components/Login';
+import SignUp from './components/signUp';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path= '/Login' element = {<Login />} ></Route>
+      <Route path= '/Signup' element = {<SignUp />} ></Route>
+      <Route path= '/Contact' element = {<Contact />} ></Route>
     </Routes>
   );
 };
