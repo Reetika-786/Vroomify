d1af154663ebdb38c28915b3e262d7ee3f455ca6
Reetika-786 <reetika848@gmail.com>
2025-12-03 22:40:05 +0530

    created manage cars

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import { Routes, Route } from 'react-router-dom'
 import AddCar from './components/addCar.jsx'
+import ManageCar from './components/manageCar.jsx'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/' element ={<AddCar />} />
+        <Route path='/manage-cars' element={<ManageCar />} />
       </Routes>
       </>
   )
